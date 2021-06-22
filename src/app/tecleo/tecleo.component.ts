51b1f173c032d2bb188ee33fca38bf638085481c
Sergio Dosil <sergiodosil@gmail.com>
2021-06-22 11:26:42 +0200

    tecleo ngmodel

--- src/app/tecleo/tecleo.component.ts
+++ src/app/tecleo/tecleo.component.ts
@@ -3,13 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tecleo',
   templateUrl: './tecleo.component.html',
-  styleUrls: ['./tecleo.component.css']
+  styleUrls: ['./tecleo.component.css'],
 })
 export class TecleoComponent implements OnInit {
+  public frase = 'En un lugar de la mancha';
+  public mensaje = 'Esperando';
+  private inicio = Date.now();
+  public loTecleado = 'Haz click en empezar';
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public empezar() {
+    console.log('Click en empezar');
+    this.mensaje = 'Vamos allá';
+    this.inicio = Date.now();
+    this.loTecleado = '';
   }
 
+  public onInput() {
+    const ahora = Date.now();
+    let tiempo = (ahora - this.inicio) / 1000;
+    if (this.frase === this.loTecleado) {
+      this.mensaje = 'Enhorabuena, has tardado ' + tiempo;
+    } else if (this.frase.startsWith(this.loTecleado)) {
+      this.mensaje = 'Vas bien ' + tiempo;
+    } else {
+      this.mensaje = 'Vas mal ' + tiempo;
+    }
+  }
 }
--- src/app/tecleo/tecleo.module.ts
+++ src/app/tecleo/tecleo.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TecleoComponent } from './tecleo.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [TecleoComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [TecleoComponent],
 })
 export class TecleoModule {}
